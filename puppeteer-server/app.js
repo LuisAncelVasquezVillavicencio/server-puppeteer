fcb1bf49f9745b8d677f3ce69eccbdd9b5811d08
LuisAncelVasquezVillavicencio <47759942+LuisAncelVasquezVillavicencio@users.noreply.github.com>
2025-02-23 20:28:22 -0500

    carga

--- puppeteer-server/app.js
+++ puppeteer-server/app.js
@@ -2,46 +2,32 @@ const express = require('express');
 const next = require('next');
 const path = require('path');
 const routes = require('./routes');
-const { WebSocketServer } = require('ws');
+const initializeSocket = require('./socket'); // Importa el módulo del socket
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });  
-const handle = app.getRequestHandler(); 
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    const server = express();
+  const server = express();
 
-    server.use((req, res, next) => {
-        req.domain = req.hostname.replace(/^www\./, '');
-        next();
-    });
+  server.use((req, res, next) => {
+    req.domain = req.hostname.replace(/^www\./, '');
+    next();
+  });
 
-    server.use('/public', express.static(path.join(__dirname, 'public')));
-
-    server.use('/', routes);
-
-    server.all('*', (req, res) => {
-        return handle(req, res);
-    });
-    
-    const httpServer = server.listen(8080, () => {
-        console.log(`🚀 Servidor corriendo en http://localhost:8080`);
-    });
-    
-    // Configurar WebSocket en el mismo servidor Express  
-    const wss = new WebSocketServer({ server: httpServer , path: '/ws' });
-    wss.on('connection', (ws, req) => {
-        console.log('Cliente conectado para recibir logs');
-        console.log('URL de conexión:', req.url);
-        
-        ws.send(JSON.stringify({ type: 'info', message: 'Conexión WebSocket establecida' }));
-
-        ws.on('message', (message) => {
-            console.log('Mensaje recibido:', message);
-        });
-
-        ws.on('close', () => {
-            console.log('Cliente WebSocket desconectado');
-        });
-    });
+  server.use('/public', express.static(path.join(__dirname, 'public')));
+  server.use('/', routes);
+  
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+  
+  // Levanta el servidor HTTP en el puerto 8080
+  const httpServer = server.listen(8080, () => {
+    console.log(`🚀 Servidor corriendo en http://localhost:8080`);
+  });
+  
+  // Inicializa el WebSocket y la escucha de logs
+  initializeSocket(httpServer);
 });
