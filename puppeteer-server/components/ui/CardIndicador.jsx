cfd198857ee924854e26489386ca400a2a140038
LuisAncelVasquezVillavicencio <47759942+LuisAncelVasquezVillavicencio@users.noreply.github.com>
2025-03-06 22:27:02 -0500

    Update CardIndicador.jsx

--- puppeteer-server/components/ui/CardIndicador.jsx
+++ puppeteer-server/components/ui/CardIndicador.jsx
@@ -1,50 +1,52 @@
 import React from 'react';
 import { Card, CardContent, Typography, Box } from '@mui/material';
+import { Paper } from '@mui/material'; 
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 function CardIndicador({ title, mainValue, extraTitleInfo , extraInfo }) {
   return (
-    <Card
-      sx={{
-        borderRadius: 2,
-        boxShadow: 3, 
-        p: 0,
-        position: 'relative',
-        overflow: 'hidden',
-        background: '#090f21',
-        border: '1px solid #8000e9',
-      }}
-    >
-      <CardContent>
-        {/* Título */}
-        <Typography variant="subtitle1" sx={{ color: '#ffffff', fontWeight: 500 }}>
-          {title}
-        </Typography>
+    <Paper variant="cosmicCard">
+      <Card
+        sx={{
+          borderRadius: 2,
+          boxShadow: 3, 
+          p: 0,
+          position: 'relative',
+          overflow: 'hidden',
+          background: '#090f21',
+        }}
+      >
+        <CardContent>
+          {/* Título */}
+          <Typography variant="subtitle1" sx={{ color: '#ffffff', fontWeight: 500 }}>
+            {title}
+          </Typography>
 
-        {/* Valor principal (puede ser número, texto, etc.) */}
-        <Typography variant="h5" sx={{ fontWeight: 600 }}>
-          {mainValue !== null && mainValue !== undefined ? mainValue : 'Cargando...'}
-        </Typography>
+          {/* Valor principal (puede ser número, texto, etc.) */}
+          <Typography variant="h5" sx={{ fontWeight: 600 }}>
+            {mainValue !== null && mainValue !== undefined ? mainValue : 'Cargando...'}
+          </Typography>
 
-        {/* Información adicional (porcentaje, texto o lo que quieras) */}
-        {extraInfo && (
-          <Box display="flex" mt={1}>
-            <Typography variant="body2" color="text.secondary">
-                {extraTitleInfo}
-            </Typography>
-            <Box
-                display="flex"
-                alignItems="center"
-                sx={{ ml: 2, color:  'success.main'  }}
-            >
-                <ArrowUpwardIcon sx={{ fontSize: '0.8rem' }} />
-                <Typography variant="body2" ml={0.5}>
-                    {extraInfo}
-                </Typography>
-            </Box>
-            </Box>
-        )}
-      </CardContent>
-    </Card>
+          {/* Información adicional (porcentaje, texto o lo que quieras) */}
+          {extraInfo && (
+            <Box display="flex" mt={1}>
+              <Typography variant="body2" color="text.secondary">
+                  {extraTitleInfo}
+              </Typography>
+              <Box
+                  display="flex"
+                  alignItems="center"
+                  sx={{ ml: 2, color:  'success.main'  }}
+              >
+                  <ArrowUpwardIcon sx={{ fontSize: '0.8rem' }} />
+                  <Typography variant="body2" ml={0.5}>
+                      {extraInfo}
+                  </Typography>
+              </Box>
+              </Box>
+          )}
+        </CardContent>
+      </Card>
+    </Paper>
   );
 }
 
